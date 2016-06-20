d96efea53f05d27b44bc4da6507f482e3c5d17da
Nick Sutterer <apotonick@gmail.com>
2016-06-20 16:09:31 +1000

    faster site by concating JS files with grunt! 🤓

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,8 +11,28 @@ module.exports = function(grunt) {
           outputStyle: 'compressed'
         },
         files: {
+          // compile into css/style.css.
           'css/style.css': 'scss/style.scss'
-        }        
+        }
+      }
+    },
+
+    uglify: {
+      jquery: {
+        files: {
+          'javascripts/jquery.min.js': 'bower_components/jquery/dist/jquery.js'
+        }
+      },
+      application: {
+        files: {
+          'javascripts/application.min.js': [
+            // "bower_components/jquery/dist/jquery.js", // FIXME: doesn't concat properly.
+            'bower_components/foundation/js/foundation.min.js',
+            'bower_components/anchor-js/anchor.js',
+            'javascripts/highlight.pack.js',
+            'bower_components/modernizr/modernizr.js'
+          ]
+        }
       }
     },
 
@@ -28,7 +48,8 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('build', ['sass', "uglify"]);
   grunt.registerTask('default', ['build','watch']);
 }
