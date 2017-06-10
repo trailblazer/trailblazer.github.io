719e9f9bcb4a2c1fe72769f4a7f67452a6e8fe57
Bhushan Lodha <bhushanlodha@gmail.com>
2017-06-10 11:14:58 +0200

    Added setup.sh file that clones all trb repos required for this project

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/trailblazer/guides ../trailblazer-guides
+git clone https://github.com/trailblazer/trailblazer ../trailblazer
+git clone https://github.com/trailblazer/trailblazer-circuit ../trailblazer-circuit
+git clone https://github.com/trailblazer/representable ../representable
