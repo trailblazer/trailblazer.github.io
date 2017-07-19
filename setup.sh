ac47850ba5798275cb51214a9b20f35243a626ae
Nick Sutterer <apotonick@gmail.com>
2017-07-19 22:01:29 +0200

    fixes problems mentioned in #111.

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ git clone https://github.com/trailblazer/guides ../trailblazer-guides
 git clone https://github.com/trailblazer/trailblazer ../trailblazer
 git clone https://github.com/trailblazer/trailblazer-circuit ../trailblazer-circuit
 git clone https://github.com/trailblazer/representable ../representable
+git clone https://github.com/apotonick/gemgem-sinatra ../gemgem-sinatra
